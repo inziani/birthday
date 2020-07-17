3464d80c1a6ecbcb626373f6ab5cc8049e1f7214
“inziani” <ValentineRobai.Inziani@gmail.com>
2020-07-17 16:18:40 +0300

    change page title

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+maleNames= ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+femaleNames= ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+userGender = ['M', 'F'];
+
+alert(userGender[0])
+  
+  
+  
